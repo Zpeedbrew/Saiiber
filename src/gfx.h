86585a293b0b8668376e8af116a988a5afd1e2a0
beemerwt <beemerwt@gmail.com>
2023-07-19 12:22:40 -0500

    Formatting fixes and added projection specialization

--- src/gfx.h
+++ src/gfx.h
@@ -4,50 +4,38 @@
 #include <ogc/gx.h>
 #include <stddef.h>
 
-enum BlendMode {
-  MODE_BLEND,
-  MODE_BLEND2,
-  MODE_BLEND3,
-  MODE_INVERT,
-  MODE_OFF
-};
+enum BlendMode { MODE_BLEND, MODE_BLEND2, MODE_BLEND3, MODE_INVERT, MODE_OFF };
 
-enum TextureMap {
-  TEX_MODEL,
-  TEX_GUI,
-  TEX_GUI2,
-  TEX_FONT,
-  TEX_NONE
-};
+enum TextureMap { TEX_MODEL, TEX_GUI, TEX_GUI2, TEX_FONT, TEX_NONE };
 
 extern int SCREEN_WIDTH, SCREEN_HEIGHT;
 extern Mtx44 projection;
 extern Mtx view;
 
 // framebuffer operations
-extern void GFX_Init();
-extern void GFX_Cleanup();
-extern void GFX_FlipBuffers(float* gpu_wait, float* vsync_wait);
-extern void GFX_CopyBuffers(u32 cnt);
-extern void GFX_Finish(bool vsync);
+void GFX_Init();
+void GFX_Cleanup();
+void GFX_FlipBuffers(float* gpu_wait, float* vsync_wait);
+void GFX_CopyBuffers(u32 cnt);
+void GFX_Finish(bool vsync);
 
 // settings
-extern void GFX_EnableAlphaTest(bool enable);
-extern void GFX_EnableCulling(bool enable);
-extern void GFX_EnableLighting(bool enable);
-extern void GFX_EnableBGColor(bool enable);
-extern void GFX_DepthRange(float near, float far);
+void GFX_EnableAlphaTest(bool enable);
+void GFX_EnableCulling(bool enable);
+void GFX_EnableLighting(bool enable);
+void GFX_EnableBGColor(bool enable);
+void GFX_DepthRange(float near, float far);
 
 // active elements
-extern void GFX_BindTexture(TextureMap texmap);
-extern void GFX_SetBlendMode(BlendMode mode);
-extern void GFX_DepthFunc(u8 func);
-extern void GFX_SetWriteBuffers(bool color, bool depth, bool depth_test);
+void GFX_BindTexture(TextureMap texmap);
+void GFX_SetBlendMode(BlendMode mode);
+void GFX_DepthFunc(u8 func);
+void GFX_SetWriteBuffers(bool color, bool depth, bool depth_test);
 
-//matrices
-extern void GFX_ModelViewMatrix(Mtx mv);
-extern void GFX_TextureMatrix(bool enable, Mtx matrix = NULL);
+// matrices
+void GFX_Projection(Mtx44 projection, int type);
+void GFX_ModelViewMatrix(Mtx model, Mtx _view = NULL);
+void GFX_TextureMatrix(bool enable, Mtx matrix = NULL);
+void GFX_OutputMatrix(Mtx matrix);
 
-extern void GFX_OutputMatrix(Mtx matrix);
-
-#endif // GFX_H
+#endif  // GFX_H
