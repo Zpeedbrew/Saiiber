b4bb6d18ee79c2210b70790f862c10eac3fcc875
beemerwt <beemerwt@gmail.com>
2023-07-19 12:26:01 -0500

    Added normals to model and fixed formatting

--- src/resource/model.cpp
+++ src/resource/model.cpp
@@ -1,15 +1,17 @@
 #include "model.h"
-#include <malloc.h>
+
 #include <gccore.h>
 #include <limits.h>
+#include <malloc.h>
+
 #include <cstring>
 
 #define ASSET_PATH "sd:/apps/saiiber/assets"
 #define TINYOBJLOADER_IMPLEMENTATION
 
-#include "tiny_obj_loader.h"
 #include "../gfx.h"
 #include "../logger.h"
+#include "tiny_obj_loader.h"
 
 tinyobj::attrib_t attrib;
 std::vector<tinyobj::shape_t> shapes;
@@ -19,20 +21,23 @@ std::string err;
 void* make_model(u32& size) {
   size_t indices = 0;
   for (const auto& shape : shapes) {
-    LOG_DEBUG("Shape %s has %d indices and %d vertices\n", shape.name.c_str(), shape.mesh.indices.size(), shape.mesh.num_face_vertices.size());
+    LOG_DEBUG("Shape %s has %d indices and %d vertices\n", shape.name.c_str(),
+              shape.mesh.indices.size(), shape.mesh.num_face_vertices.size());
     indices += shape.mesh.indices.size();
   }
 
-  LOG_DEBUG("Initially loaded model with:\n\tIndices: %d\n\tVertices: %d\n\tTexCoords: %d\n",
-    indices, attrib.vertices.size(), attrib.texcoords.size());
+  LOG_DEBUG(
+      "Initially loaded model with:\n\tIndices: %d\n\tVertices: "
+      "%d\n\tTexCoords: %d\n",
+      indices, attrib.vertices.size(), attrib.texcoords.size());
 
   size_t szVertices = indices * 3 * sizeof(s16);
   size_t szTexCoords = indices * 2 * sizeof(u16);
+  size_t szNormals = indices * 3 * sizeof(s16);
   size_t szColor = indices * 4 * sizeof(u8);
-  size_t total = 3 + szVertices + szTexCoords + szColor + 63;
+  size_t total = 3 + szVertices + szTexCoords + szColor + szNormals + 63;
 
-  if (total % 64 > 0)
-    total += 64 - (total % 64);
+  if (total % 64 > 0) total += 64 - (total % 64);
 
   LOG_DEBUG("Initial size: %d\n", total);
 
@@ -52,7 +57,9 @@ void* make_model(u32& size) {
                         attrib.vertices[3 * index.vertex_index + 1] * 32768,
                         attrib.vertices[3 * index.vertex_index + 2] * 32768);
 
-        GX_Color4u8(0xFF, 0xFF, 0xFF, 0xFF);
+        GX_Normal3s16(attrib.normals[3 * index.normal_index + 0] * 32768,
+                      attrib.normals[3 * index.normal_index + 1] * 32768,
+                      attrib.normals[3 * index.normal_index + 2] * 32768);
 
         GX_TexCoord2u16(attrib.texcoords[2 * index.texcoord_index + 0] * 65536,
                         attrib.texcoords[2 * index.texcoord_index + 1] * 65536);
@@ -72,12 +79,12 @@ void* make_model(u32& size) {
   return display;
 }
 
-Model::Model(const char *filename) {
+Model::Model(const char* filename) {
   char path[PATH_MAX];
   snprintf(path, PATH_MAX, "%s%s", ASSET_PATH, filename);
   LOG_DEBUG("Loading model %s\n", path);
 
-  std::ifstream file(path, std::ios::binary);  
+  std::ifstream file(path, std::ios::binary);
   if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &err, &file)) {
     file.close();
     return;
@@ -95,27 +102,20 @@ Model::Model(const char *filename) {
   Although the saved allocation can be noticable there, too.
   For large chunks of memory, it makes a major difference.
 */
-struct membuf: std::streambuf {
-  membuf(char* base, std::ptrdiff_t n) {
-    this->setg(base, base, base + n);
-  }
+struct membuf : std::streambuf {
+  membuf(char* base, std::ptrdiff_t n) { this->setg(base, base, base + n); }
 };
 
 Model::Model(const uint8_t* data, u32 size) {
   membuf buf((char*)data, size);
   std::istream stream(&buf);
-  
-  if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &err, &stream))
-    return;
+
+  if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &err, &stream)) return;
 
   display = make_model(this->size);
   loaded = display != NULL;
 }
 
-Model::~Model() {
-  free(display);
-}
+Model::~Model() { free(display); }
 
-void Model::render() {
-  GX_CallDispList(display, size);
-}
+void Model::render() { GX_CallDispList(display, size); }
