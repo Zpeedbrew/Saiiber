0c18f43ec6ad6b174d7dc55748f5dd3bdb88210e
beemerwt <beemerwt@gmail.com>
2023-08-04 15:02:39 -0500

    Added proper rotate function

--- src/transform.cpp
+++ src/transform.cpp
@@ -1,37 +1,62 @@
 #include "transform.h"
-#include <ogc/gx.h>
 
-guVector copy(const guVector v) {
-  return { v.x, v.y, v.z };
-}
+guVector VEC_FORWARD = {0.0f, 0.0f, -1.0f};
+guVector VEC_UP = {0.0f, 1.0f, 0.0f};
 
-guVector Transform::UP = { 0.0f, 1.0f, 0.0f };
-guVector Transform::FORWARD = { 0.0f, 0.0f, -1.0f };
+Transform::Transform()
+    : rotation{0, 0, 0, 1},
+      scale{1, 1, 1},
+      position{0, 0, 0},
+      matrix{{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}} {}
 
 bool Transform::intersects(float x, float y, float width, float height) {
   return (position.x + scale.x >= x && position.x <= x + width) &&
-    (position.y + scale.y >= y && position.y <= y + height);
+         (position.y + scale.y >= y && position.y <= y + height);
 }
 
-bool Transform::intersects(float x, float y, float z, float width, float height, float depth) {
-  return intersects(x, y, width, height)
-    && (position.z + scale.z >= z && position.z <= z + depth);
+bool Transform::intersects(float x, float y, float z, float width, float height,
+                           float depth) {
+  return intersects(x, y, width, height) &&
+         (position.z + scale.z >= z && position.z <= z + depth);
 }
 
 bool Transform::intersects(Transform other) {
   return intersects(other.position.x, other.position.y, other.position.z,
-    other.scale.x, other.scale.y, other.scale.z);
+                    other.scale.x, other.scale.y, other.scale.z);
 }
-
-guVector xAxis = { 1.0f, 0.0f, 0.0f };
-guVector yAxis = { 0.0f, 1.0f, 0.0f };
-guVector zAxis = { 0.0f, 0.0f, 1.0f };
 
 void Transform::update() {
   guMtxIdentity(matrix);
   guMtxTrans(matrix, position.x, position.y, position.z);
-  guMtxRotAxisDeg(matrix, &xAxis, rotation.x);
-  guMtxRotAxisDeg(matrix, &yAxis, rotation.y);
-  guMtxRotAxisDeg(matrix, &zAxis, rotation.z);
+  guMtxRotate(matrix, rotation);
   guMtxScale(matrix, scale.x, scale.y, scale.z);
+}
+
+guVector Transform::forward() { return rotation * VEC_FORWARD; }
+
+guVector Transform::right() {
+  guVector right;
+  guVector front = forward();
+
+  guVecCross(&front, &VEC_UP, &right);
+  guVecNormalize(&right);
+  return right;
+}
+
+void Transform::rotate(float yaw, float pitch, float roll) {
+  guQuaternion y = {0, sinf(DegToRad(yaw) / 2.0f), 0,
+                    cosf(DegToRad(yaw) / 2.0f)};
+
+  guQuaternion p = {sinf(DegToRad(pitch) / 2.0f), 0, 0,
+                    cosf(DegToRad(pitch) / 2.0f)};
+
+  guQuaternion r = {0, 0, sinf(DegToRad(roll) / 2.0f),
+                    cosf(DegToRad(roll) / 2.0f)};
+
+  // Tested and works as intended and very optimally
+  guQuaternion q = { 0, 0, 0, 1 };
+  guQuatMultiply(&y, &p, &q);
+  guQuatMultiply(&q, &r, &q);
+  guQuatMultiply(&rotation, &q, &q);
+  guQuatNormalize(&q, &rotation);
 }
--- src/transform.h
+++ src/transform.h
@@ -1,23 +1,35 @@
 #ifndef TRANSFORM_H
 #define TRANSFORM_H
-#include <ogc/gu.h>
+#include "exmath.h"
+
+extern guVector VEC_FORWARD;
+extern guVector VEC_UP;
 
 class Transform {
-public:
-  static guVector UP;
-  static guVector FORWARD;
+ public:
+  guQuaternion rotation;
+  guVector scale;
+  guVector position;
 
-  guVector position = { 0, 0, 0 };
-  guVector rotation = { 0, 0, 0 };
-  guVector scale = { 1.0f, 1.0f, 1.0f };
-  
   Mtx matrix;
 
+  Transform();
+  
+  guVector forward();
+  guVector right();
+
+  // We have an update for the final matrix update
+  // That way we don't have to provide a method to calculate the matrix
+  // which would have been used in the render thread, rather than the update
+  void update();
+
   bool intersects(Transform other);
-  bool intersects(float x, float y, float z, float width, float height, float depth);
+  bool intersects(float x, float y, float z, float width, float height,
+                  float depth);
   bool intersects(float x, float y, float width, float height);
 
-  void update();
+  // Rotates transform based on euler angles in degrees
+  void rotate(float yaw, float pitch, float roll);
 };
 
-#endif // TRANSFORM_H
+#endif  // TRANSFORM_H
