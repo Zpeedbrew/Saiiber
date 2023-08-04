b10eff63be261026f962429a340157a3297f3d98
beemerwt <beemerwt@gmail.com>
2023-08-04 10:23:25 -0500

    Added fx obj and vec/quat operators

--- src/exmath.cpp
+++ src/exmath.cpp
@@ -1,9 +1,61 @@
 #include "exmath.h"
 
-#include <wiiuse/wpad.h>
 
-#include <chrono>
-#include <thread>
+float minf(float a, float b) {
+  if (a < b) return a;
+  return b;
+}
+
+float maxf(float a, float b) {
+  if (a > b) return a;
+  return b;
+}
+
+guQuaternion guQuatConjugate(guQuaternion& q) {
+  return {-q.x, -q.y, -q.z, q.w};
+}
+
+void _guQuatMtx(guQuaternion& q, Mtx& mtx) {
+  double sqw = q.w * q.w;
+  double sqx = q.x * q.x;
+  double sqy = q.y * q.y;
+  double sqz = q.z * q.z;
+
+  // inverse square length is only required if not already normalised
+  // since sqw + sqx + sqy + sqz = 1 / invs * invs
+  double invs = 1 / (sqx + sqy + sqz + sqw);
+  mtx[0][0] = (sqx - sqy - sqz + sqw) * invs;
+  mtx[1][1] = (-sqx + sqy - sqz + sqw) * invs;
+  mtx[2][2] = (-sqx - sqy + sqz + sqw) * invs;
+
+  double tmp1 = q.x * q.y;
+  double tmp2 = q.z * q.w;
+  mtx[1][0] = 2.0 * (tmp1 + tmp2) * invs;
+  mtx[0][1] = 2.0 * (tmp1 - tmp2) * invs;
+
+  tmp1 = q.x * q.z;
+  tmp2 = q.y * q.w;
+  mtx[2][0] = 2.0 * (tmp1 - tmp2) * invs;
+  mtx[0][2] = 2.0 * (tmp1 + tmp2) * invs;
+
+  tmp1 = q.y * q.z;
+  tmp2 = q.x * q.w;
+  mtx[2][1] = 2.0 * (tmp1 + tmp2) * invs;
+  mtx[1][2] = 2.0 * (tmp1 - tmp2) * invs;
+}
+
+void _guQuatAdd(guQuaternion& q1, guQuaternion& q2, guQuaternion& dst) {
+  dst.x = q1.x + q2.x;
+  dst.y = q1.y + q2.y;
+  dst.z = q1.z + q2.z;
+  dst.w = q1.w + q2.w;
+}
+
+void _guQuatAdd(guVector& v, guQuaternion& q, guQuaternion& dst) {
+  // create pseudo quaternion from V
+  guQuaternion vq{v.x, v.y, v.z, 0};
+  _guQuatAdd(vq, q, dst);
+}
 
 guVector operator+(const guVector& left, const guVector& right) {
   return {left.x + right.x, left.y + right.y, left.z + right.z};
@@ -13,16 +65,51 @@ guVector operator-(const guVector& left, const guVector& right) {
   return {left.x - right.x, left.y - right.y, left.z - right.z};
 }
 
-guVector operator/(const guVector& left, const float right) {
-  return {left.x / right, left.y / right, left.z / right};
+guVector operator/(const guVector& v, const float scalar) {
+  return { v.x / scalar, v.y / scalar, v.z / scalar};
 }
 
-guVector operator*(const guVector& left, const float scalar) {
-  return {left.x * scalar, left.y * scalar, left.z * scalar};
+guVector operator*(const float scalar, const guVector& v) {
+  return {scalar * v.x, scalar * v.y, scalar * v.z};
+}
+
+guVector operator*(const guQuaternion& left, const guVector& right) {
+  guVector xyz = {left.x, left.y, left.z};
+
+  guVector t;
+  guVecCross(&xyz, &right, &t);
+  t *= 2.0f;
+
+  guVector ct;
+  guVecCross(&xyz, &t, &ct);
+
+  return right + (left.w * t) + ct;
+}
+
+bool operator==(guVector& left, guVector& right) {
+  return left.x == right.x && left.y == right.y && left.z == right.z;
 }
 
 bool operator!=(guVector& left, guVector& right) {
-  return left.x != right.x || left.y != right.y || left.z != right.z;
+  return !(left == right);
+}
+
+guVector& operator*=(guVector& v, const float scalar) {
+  v.x *= scalar;
+  v.y *= scalar;
+  v.z *= scalar;
+  return v;
+}
+
+guVector& operator*=(guVector& left, guVector& right) {
+  left.x *= right.x;
+  left.y *= right.y;
+  left.z *= right.z;
+  return left;
+}
+
+guVector operator-(const guVector& v) {
+  return { -v.x, -v.y, -v.z };
 }
 
 guVector& operator+=(guVector& left, guVector right) {
@@ -39,30 +126,7 @@ guVector& operator-=(guVector& left, guVector right) {
   return left;
 }
 
-int millis() {
-  auto time = std::chrono::system_clock::now();
-  auto since_epoch = time.time_since_epoch();
-  auto millis =
-      std::chrono::duration_cast<std::chrono::milliseconds>(since_epoch);
-
-  return millis.count();
-}
-
-void sleep_for(int ms) {
-  std::this_thread::sleep_for(std::chrono::milliseconds(ms));
-}
-
-void pressA(int wiimote) {
-  while (1) {
-    WPAD_ScanPads();
-    u32 gcPressed = WPAD_ButtonsDown(wiimote);
-    if (gcPressed & WPAD_BUTTON_A) break;
-
-    sleep_for(50);
-  }
-}
-
 float angleInRadians(int lo, int hi, long measured) {
   float x = (hi - lo);
-  return (float)(measured / x) * PI;
+  return (float)(measured / x) * M_PI;
 }
--- src/exmath.h
+++ src/exmath.h
@@ -4,35 +4,54 @@
  * Extended Math (exmath)
  * Author: TheSunCat
  * A collection of useful mathematical functions and constants.
-*/
+ */
 
-#include <gccore.h>
 #include <math.h>
+#include <ogc/gu.h>
 
-const double PI = 3.14159265358979323846;
+#undef guQuatMtx
+#undef guQuatAdd
+
+#define guQuatMtx _guQuatMtx
+#define guQuatAdd _guQuatAdd
+
+/* Typically, in a formal Mathematical setting, min/max would mean "magnitude,"
+ * or the distance from 0. However, the < and > operators in C++ specifically
+ * only compare the leftmost and rightmost. */
+// LEFTMOST value
+float minf(float a, float b);
+// RIGHTMOST value
+float maxf(float a, float b); 
+
+guQuaternion guQuatConjugate(guQuaternion& q);
+void _guQuatMtx(guQuaternion& q, Mtx& mtx);
+
+void _guQuatAdd(guQuaternion& q1, guQuaternion& q2, guQuaternion& dst);
+void _guQuatAdd(guVector& v, guQuaternion& q, guQuaternion& dst);
+
+guVector operator-(const guVector& v);
 
 guVector operator+(const guVector& left, const guVector& right);
 guVector operator-(const guVector& left, const guVector& right);
-guVector operator/(const guVector& left, const float right);
-guVector operator*(const guVector& left, const float scalar);
+guVector operator/(const guVector& v, const float scalar);
+guVector operator*(const float scalar, const guVector& v);
 
+guVector operator*(const guQuaternion& left, const guVector& right);
+
+bool operator==(guVector& left, guVector& right);
 bool operator!=(guVector& left, guVector& right);
+
+guVector& operator*=(guVector& v, const float scalar);
+guVector& operator*=(guVector& left, guVector& right);
 
 guVector& operator+=(guVector& left, guVector right);
 guVector& operator-=(guVector& left, guVector right);
 
-int millis();
-
-void sleep_for(int ms);
-
-// Block until A is pressed
-void pressA(int wiimote = 0);
-
-/* Nunchuk accelerometer value to radian conversion.   Normalizes the measured value
- * into the range specified by hi-lo.  Most of the time lo should be 0, and hi should be 1023.
- * lo         the lowest possible reading from the Nunchuk
- * hi         the highest possible reading from the Nunchuk
- * measured   the actual measurement */
+/* Nunchuk accelerometer value to radian conversion.   Normalizes the measured
+ * value into the range specified by hi-lo.  Most of the time lo should be 0,
+ * and hi should be 1023. lo         the lowest possible reading from the
+ * Nunchuk hi         the highest possible reading from the Nunchuk measured the
+ * actual measurement */
 float angleInRadians(int lo, int hi, long measured);
 
-#endif // EX_MATH
+#endif  // EX_MATH
