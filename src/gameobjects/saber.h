d03edef4da495aa9b351a8fd0d72d487a98a22d5
guinea7pig <145406121+guinea7pig@users.noreply.github.com>
2024-10-07 10:14:59 -0400

    Update saber.h

--- src/gameobjects/saber.h
+++ src/gameobjects/saber.h
@@ -1,1 +1,23 @@
+#ifndef saber_H
+#define saber_H
+#include "gameobject.h"
 
+class Model;
+
+class Block : public GameObject {
+private:
+  Mtx texMtx;
+
+public:
+  static Model* model;
+
+  Saber();
+  ~Saber();
+  void update(f32 deltatime);
+  void render();
+
+  void setTexMtx(s16 x, s16 y, s16 w, s16 h);
+//is this the width. hight
+};
+
+#endif // BLOCK_H
