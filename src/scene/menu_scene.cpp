42959d8e043ade3929c6737ab88f763936540db3
guinea7pig <145406121+guinea7pig@users.noreply.github.com>
2024-10-08 12:35:22 -0400

    Update menu_scene.cpp

--- src/scene/menu_scene.cpp
+++ src/scene/menu_scene.cpp
@@ -1,4 +1,5 @@
 #include "menu_scene.h"
+
 // i thinks is supposed to be for the main menu idk how to do text in gx
 //i think this has text i think the best way is to draw a texture on a qaud
 MenuScene::MenuScene() {
