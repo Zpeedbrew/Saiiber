471408e17f20080f5009da1e0bbab7b0061653db
Ryan Murphy <toadgit76@gmail.com>
2025-04-18 10:55:27 -0400

    Finally fixed it, I hope.

--- build/beatmap.d
+++
@@ -1,9 +0,0 @@
-beatmap.o: /home/kai/Saiiber/src/resource/beatmap.cpp \
- /home/kai/Saiiber/src/resource/beatmap.h \
- /opt/devkitpro/libogc/include/gctypes.h \
- /home/kai/Saiiber/src/resource/../json_struct.h \
- /home/kai/Saiiber/src/resource/../logger.h
-/home/kai/Saiiber/src/resource/beatmap.h:
-/opt/devkitpro/libogc/include/gctypes.h:
-/home/kai/Saiiber/src/resource/../json_struct.h:
-/home/kai/Saiiber/src/resource/../logger.h:
