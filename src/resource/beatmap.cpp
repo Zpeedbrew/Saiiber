81a794a82a0707c585819d3bd8f3b4dd7fcbf10a
guinea7pig <145406121+guinea7pig@users.noreply.github.com>
2025-04-30 07:44:56 -0400

    Update beatmap.cpp

--- src/resource/beatmap.cpp
+++ src/resource/beatmap.cpp
@@ -1,78 +1,69 @@
 #include "beatmap.h"
 #include "../logger.h"
 Mode ModeFromString(const char*name) {
-  if (strcmp(name,"Standard")==0)
-    return Mode::Standard;
-  else if (strcmp(name,"OneSaber")==0)
-    return Mode::OneSaber;
-  /*
-  else if (strcmp(name, "NoArrows") == 0)
-    return Mode::NoArrows;
-  else if (strcmp(name, "360Degree") == 0)
-    return Mode::ThreeSixty;
-  else if (strcmp(name, "90Degree") == 0)
-    return Mode::Ninety;
-  else if (strcmp(name, "Lightshow") == 0)
-    return Mode::Lightshow;
-  else if (strcmp(name, "Lawless") == 0)
-    return Mode::Lawless;
-  else
-    return Mode::Unknown;
+if (strcmp(name,"Standard")==0)
+return Mode::Standard;
+else if (strcmp(name,"OneSaber")==0)
+return Mode::OneSaber;
+/*
+else if (strcmp(name,"NoArrows")==0)
+return Mode::NoArrows;
+else if (strcmp(name,"360Degree")==0)
+return Mode::ThreeSixty
+else if (strcmp(name,"90Degree")==0)
+return Mode::Ninety;
+else if (strcmp(name,"Lightshow")==0)
+return Mode::Lightshow;
+else if (strcmp(name,"Lawless")==0)
+return Mode::Lawless;
+else
+return Mode::Unknown;
 */
 return Mode::OneSaber;
 }
-
-Rank RankFromString(const char*name) {
-  if(strcmp(name,"Easy")==0)
-  return Rank::Easy;
-  else if(strcmp(name,"Normal")==0)
-  return Rank::Normal;
-  else if(strcmp(name,"Hard")==0)
-  return Rank::Hard;
-  else if(strcmp(name,"Expert")==0)
-  return Rank::Expert;
-  else if(strcmp(name,"imposible")==0)
-  return Rank::ExpertPlus;
-  else
-  return Rank::Normal;
+Rank RankFromString(const char*name){
+if(strcmp(name,"Easy")==0)
+return Rank::Easy;
+else if(strcmp(name,"Normal")==0)
+return Rank::Normal;
+else if(strcmp(name,"Hard")==0)
+return Rank::Hard;
+else if(strcmp(name,"Expert")==0)
+return Rank::Expert;
+else if(strcmp(name,"imposible")==0)
+return Rank::ExpertPlus;
+else
+return Rank::Normal;
 }
-
 static std::string beatmapDirectory;
-
 int LoadDifficulty(std::string&path,Difficulty&difficulty){
-  FILE*file=fopen(path.c_str(),"r");
-  if(file==NULL){
-  LOG_ERROR("Failed to open difficulty file:%s\n",path.c_str());
-  return -1;
-  }
-
-  fseek(file,0,SEEK_END);
-  size_t size=ftell(file);
-  fseek(file,0,SEEK_SET);
-
-  char*data=new char[size];
-  fread(data,1,size,file);
-  fclose(file);
-
-  JS::Map map;
-  JS::ParseContext parseContext(data,size,map);
-  if (parseContext.error !=JS::Error::NoError) {
-    LOG_ERROR("Failed to parse Json:\n%s\n",parseContext.makeErrorString().c_str());
-    return -1;
-  }
-
-  difficulty = map.castTo<Difficulty>(parseContext);
-  if (parseContext.error != JS::Error::NoError) {
-    LOG_ERROR("Failed to get Difficulty from %s:\n%s\n", path.c_str(),
-              parseContext.makeErrorString().c_str());
-    return -1;
-  }
-
-  delete[] data;
-  return 0;
+FILE*file=fopen(path.c_str(),"r");
+if(file==NULL){
+LOG_ERROR("Failed to open difficulty file:%s\n",path.c_str());
+return -1;
 }
-
-Beatmap::Beatmap(std::string directory, BeatmapInfo info)
+fseek(file,0,SEEK_END);
+size_t size=ftell(file);
+fseek(file,0,SEEK_SET);
+char*data=new char[size];
+fread(data,1,size,file);
+fclose(file);
+JS::Map map;
+JS::ParseContext parseContext(data,size,map);
+if (parseContext.error !=JS::Error::NoError) {
+LOG_ERROR("Failed to parse Json:\n%s\n",parseContext.makeErrorString().c_str());
+return -1;
+}
+difficulty = map.castTo<Difficulty>(parseContext);
+if(parseContext.error != JS::Error::NoError){
+LOG_ERROR("Failed to get Difficulty from %s:\n%s\n", path.c_str(),
+parseContext.makeErrorString().c_str());
+return -1;
+}
+delete[] data;
+return 0;
+}
+Beatmap::Beatmap(std::string directory,BeatmapInfo info)
     : directory(directory), info(info) {}
 
 int Beatmap::loadMap(Mode mode, Rank rank) {
