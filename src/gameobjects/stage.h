f3bb2ee5af1acf6c15b62ce8f23fcd9cbdbb8fa9
guinea7pig <145406121+guinea7pig@users.noreply.github.com>
2025-04-30 07:26:51 -0400

    Update stage.h

--- src/gameobjects/stage.h
+++ src/gameobjects/stage.h
@@ -4,13 +4,12 @@
 class Model;
 class Stage:public GameObject {
 private:
-  static std::unique_ptr<Model>stagemodel;
-  static std::unique_ptr<Model>fxmodel;
-  Transform fxtransform;
-  glm::mat4 texMtx;
+static std::unique_ptr<Model>stagemodel;
+static std::unique_ptr<Model>fxmodel;
+Transform fxtransform;glm::mat4 texMtx;
 public:
-  Stage();
-  void update(f32 deltatime);
-  void render();
+Stage();
+void update(f32 deltatime);
+void render();
 };
 #endif
