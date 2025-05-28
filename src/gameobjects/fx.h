d64d904f2d5e1e5ce283f87a9acf21bfc7fe16fa
guinea7pig <145406121+guinea7pig@users.noreply.github.com>
2025-05-28 09:44:43 -0400

    Update fx.h

--- src/gameobjects/fx.h
+++ src/gameobjects/fx.h
@@ -1,1 +1,14 @@
-
+#ifndef FX_H
+#define FX_H
+#include "gameobject.h"
+class Model;
+class FX:public GameObject {
+private:
+static std::unique_ptr<Model> model;
+glm::mat4 texMtx;
+public:
+FX();
+void update(f32 deltatime);
+void render();
+};
+#endif // BLOCK_H
