eb98eb95dff100c946bd01edaaef1e5898afe255
guinea7pig <145406121+guinea7pig@users.noreply.github.com>
2026-05-08 08:07:41 -0400

    Update input.h

--- source/input.h
+++ source/input.h
@@ -1,1 +1,11 @@
+#ifndef INPUT_H
+#define INPUT_H
 
+#include <wiiuse/wpad.h>
+
+void InitInput();
+void UpdateInput();
+bool ButtonPressed(int chan, int button);
+bool ButtonHeld(int chan, int button);
+
+#endif
