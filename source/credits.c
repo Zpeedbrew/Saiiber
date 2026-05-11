02d47b6f7c84937dc5373e2d86cab74ad7622725
guinea7pig <145406121+guinea7pig@users.noreply.github.com>
2026-05-11 14:08:05 -0400

    Update credits.c

--- source/credits.c
+++ source/credits.c
@@ -37,16 +37,16 @@ int o() {
         GRRLIB_SetLightAmbient(0x333333FF);
         GRRLIB_2dMode();
         //title text 
-        GRRLIB_Printf((640-(21*21))/2, 20, tex_font, RED, 5, "credits");
-        GRRLIB_Printf((640-(16*14))/2, 400, tex_font, RED, 2, "return to home");
+        GRRLIB_Printf(100, 20, tex_font, RED, 5, "credits");
+        GRRLIB_Printf(100, 400, tex_font, RED, 2, "return to home");
         //need to button stuff eventuly.
         //exit(0)
-        GRRLIB_Printf((640, 100, tex_font, RED, 3, "devolped by Guinea7pig, Beemer,");
-        GRRLIB_Printf((640-(16*14))/2, 150, tex_font, RED, 3, "settings");
-        GRRLIB_Printf((640-(16*14))/2, 200, tex_font, RED, 3, "thanks to Crayon2000 and theGRRlIB dev team + the exsample projects");
-        GRRLIB_Printf((640-(16*14))/2, 250, tex_font, RED, 3, "thanks to the wii hombrewing community");
-        GRRLIB_Printf((640-(16*14))/2, 300, tex_font, RED, 3, "check out GRRLIB at https://github.com/GRRLIB/GRRLIB");
-        GRRLIB_Printf((640-(16*14))/2, 350, tex_font, RED, 3, "check out source code at https://github.com/Zpeedbrew/Saiiber/");
+        GRRLIB_Printf(640, 100, tex_font, RED, 3, "devolped by Guinea7pig, Beemer,");
+        GRRLIB_Printf(100, 150, tex_font, RED, 3, "settings");
+        GRRLIB_Printf(100, 200, tex_font, RED, 3, "thanks to Crayon2000 and theGRRlIB dev team + the exsample projects");
+        GRRLIB_Printf(100, 250, tex_font, RED, 3, "thanks to the wii hombrewing community");
+        GRRLIB_Printf(100, 300, tex_font, RED, 3, "check out GRRLIB at https://github.com/GRRLIB/GRRLIB");
+        GRRLIB_Printf(100, 350, tex_font, RED, 3, "check out source code at https://github.com/Zpeedbrew/Saiiber/");
         //image displaying 
         //GRRLIB_Draw_img(xpos, ypos,img,scale,scale,rot,0xFFFFFFFF)
 
