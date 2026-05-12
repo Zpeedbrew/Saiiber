571de85784fd5dd1a751baadf5fec768d5d0734e
guinea7pig <145406121+guinea7pig@users.noreply.github.com>
2026-05-12 07:46:33 -0400

    Update main.c

--- source/main.c
+++ source/main.c
@@ -23,7 +23,7 @@
 
 #define version 1.6
 #define RED 0xFF0000FF
-#define WHITE 0xFF0000FF
+#define WHITE 0xFFFFFFFF
 #define BLUE 0x0000FFFF
 #define BLACK 0x000000FF
 
@@ -43,7 +43,7 @@ int main() {
     GRRLIB_InitTileSet(tex_font, 11, 24, 32);
     GRRLIB_InitTileSet(tex_beon, 11, 24, 32); //tileset probs wrong
     //image init
-GRRLIB_texImg *tex_cur = GRRLIB_LoadTexture(hand_png);
+   GRRLIB_texImg *tex_cur = GRRLIB_LoadTexture(hand_png);
     GRRLIB_texImg *tex_dis = GRRLIB_LoadTexturePNG(discord_png);
     GRRLIB_texImg *tex_gr = GRRLIB_LoadTexturePNG(grrlib_logo_png);
     GRRLIB_texImg *tex_qr = GRRLIB_LoadTexturePNG(qr_png);
@@ -60,11 +60,11 @@ GRRLIB_texImg *tex_cur = GRRLIB_LoadTexture(hand_png);
         if(WPAD_ButtonsDown(0) & WPAD_BUTTON_MINUS) GRRLIB_ScrShot("sd:/saiiber.png");
         GRRLIB_SetLightAmbient(0x333333FF);
         //title text 
-        GRRLIB_Printf(99, 20, tex_font, RED, 5, "Saii"); //((640-(21*21))/2
+        GRRLIB_Printf(99, 20, tex_font, RED, 5, "Saii");
         GRRLIB_Printf(315, 20, tex_font, BLUE, 5, "ber");
         //menu text
         GRRLIB_Printf(10, 150, tex_font, RED, 2, "play");
-        GRRLIB_Printf(0, 200, tex_font, RED, 2, "settings");
+        GRRLIB_Printf(10, 200, tex_font, RED, 2, "settings");
         GRRLIB_Printf(10, 250, tex_font, RED, 2, "quick play");
         GRRLIB_Printf(10, 300, tex_font, RED, 2, "credits");
         GRRLIB_Printf(10, 400, tex_font, RED, 2, "exit");
@@ -73,34 +73,12 @@ GRRLIB_texImg *tex_cur = GRRLIB_LoadTexture(hand_png);
         //images
         GRRLIB_DrawImg(10, 400,tex_dis,0,1,1,WHITE);
         GRRLIB_DrawImg(30, 400,tex_qr,0,1,1,WHITE);
-        GRRLIB_DrawImg(315, 400 ,tex_gr,0,1,1,WHITE);    
+        GRRLIB_DrawImg(315, 400 ,tex_gr,0,0.1,0.1,WHITE);    
         //debug text neeed to make togglable
-        GRRLIB_Printf(10, 30, tex_font, RED, 1,"fps:");
-        //GRRLIB_Printf((640-(16*14))/2, 30, tex_font, BLUE, 1, fps);
-        GRRLIB_Printf(500, 40, tex_font, RED, 1,"cur x");
-        //GRRLIB_Printf((640-(16*14))/2,40400, tex_font, BLUE, 1, curx);
-         GRRLIB_Printf(500, 50, tex_font, RED, 1,"cur y");
-        //GRRLIB_Printf((640-(16*14))/2, 400, tex_font, BLUE, 1,cury);
-
-//this code snippet is from https://github.com/GRRLIB/GRRLIB/blob/master/examples/basic_drawing/source/main.c
-   /* static u8 CalculateFrameRate(void) {
-    static u8 frameCount = 0;
-    static u32 lastTime;
-    static u8 FPS = 0;
-    const u32 currentTime = ticks_to_millisecs(gettime());
-
-    frameCount++;
-    if(currentTime - lastTime > 1000) {
-        lastTime = currentTime;
-        FPS = frameCount;
-        frameCount = 0;
-    }
-    return FPS;
-}
-        */
-
-        //
-
+        GRRLIB_Printf(0, 30, tex_font, RED, 1,"fps: %d", FPS);
+        GRRLIB_Printf(500, 30, tex_font, RED, 1,"curx: %d", cursorX);
+        GRRLIB_Printf(500, 50, tex_font, RED, 1,"cury %d", curesorY);
+        FPS = CalculateFrameRate();
         GRRLIB_Render();
     }
     //free textures
@@ -115,4 +93,19 @@ GRRLIB_texImg *tex_cur = GRRLIB_LoadTexture(hand_png);
     GRRLIB_Exit(); // Be a good boy, clear the memory allocated by GRRLIB
 
     exit(0);
+}
+//this code snippet is from https://github.com/GRRLIB/GRRLIB/blob/master/examples/basic_drawing/source/main.c
+  /* static u8 CalculateFrameRate(void) {
+    static u8 frameCount = 0;
+    static u32 lastTime;
+    static u8 FPS = 0;
+    const u32 currentTime = ticks_to_millisecs(gettime());
+
+    frameCount++;
+    if(currentTime - lastTime > 1000) {
+        lastTime = currentTime;
+        FPS = frameCount;
+        frameCount = 0;
+    }
+    return FPS;
 }
