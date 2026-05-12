1ee352a0696026094157a2833b0356212c18eb08
guinea7pig <145406121+guinea7pig@users.noreply.github.com>
2026-05-12 09:05:44 -0400

    Update credits.c

--- source/credits.c
+++ source/credits.c
@@ -8,39 +8,29 @@
 #define BLUE 0x0000FFFF
 #include "Letter_Gothic_Std_14_Bold_png.h"
 #include "back_png.h"
+#include "hand_png.h"
 //grlib logo
 
 int o() {
-    int camZ=13.0f;
-
     GRRLIB_Init();
     WPAD_Init();
-
-
     GRRLIB_texImg *tex_font = GRRLIB_LoadTexture(Letter_Gothic_Std_14_Bold_png);
     GRRLIB_texImg *tex_back = GRRLIB_LoadTexture(back_png);
-
+    GRRLIB_texImg *tex_cur = GRRLIB_LoadTexture(hand_png);
     GRRLIB_InitTileSet(tex_font, 11, 24, 32);
-
-
     GRRLIB_Settings.antialias = true;
-
     GRRLIB_SetBackgroundColour(0x00, 0x00, 0x00, 0xFF);
-
+    GRRLIB_SetLightAmbient(0x333333FF);
     while(1) {
         GRRLIB_2dMode();
         WPAD_ScanPads();
         //if(WPAD_ButtonsDown(0) & WPAD_BUTTON_PLUS) break; //goes back to home i hope
         if(WPAD_ButtonsDown(0) & WPAD_BUTTON_HOME) break; //goes back to home i hope
-
-        GRRLIB_Camera3dSettings(0.0f,0.0f,camZ, 0,1,0, 0,0,0);
-        GRRLIB_SetLightAmbient(0x333333FF);
         GRRLIB_2dMode();
         //title text 
         GRRLIB_Printf(100, 20, tex_font, RED, 5, "credits");
         GRRLIB_Printf(100, 400, tex_font, RED, 2, "return to home");
         //need to button stuff eventuly.
-        //exit(0)
         GRRLIB_Printf(640, 100, tex_font, RED, 3, "devolped by Guinea7pig, Beemer,");
         GRRLIB_Printf(100, 150, tex_font, RED, 3, "settings");
         GRRLIB_Printf(100, 200, tex_font, RED, 3, "thanks to Crayon2000 and theGRRlIB dev team + the exsample projects");
@@ -48,16 +38,7 @@ int o() {
         GRRLIB_Printf(100, 300, tex_font, RED, 3, "check out GRRLIB at https://github.com/GRRLIB/GRRLIB");
         GRRLIB_Printf(100, 350, tex_font, RED, 3, "check out source code at https://github.com/Zpeedbrew/Saiiber/");
         //image displaying 
-        //GRRLIB_Draw_img(xpos, ypos,img,scale,scale,rot,0xFFFFFFFF)
-
-        //debug text neeed to make togglable
-        /*
-        GRRLIB_Printf((640-(16*14))/2, 350, tex_font, 0xFF0000FF, 2, "current version:1.6");
-        GRRLIB_Printf((640-(16*14))/2, 350, tex_font, 0xFF0000FF, 2, "rendering engine:GRRLIB");
-        */
-
-
-
+        //GRRLIB_Draw_img(xpos, ypos,img,scale,scale,rot,0xFFFFFFFF);
         GRRLIB_Render();
     }
     GRRLIB_FreeTexture(tex_font);
