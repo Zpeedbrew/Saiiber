a1be0b384dfd83354d2d8da346e59cedd7c0e99f
guinea7pig <145406121+guinea7pig@users.noreply.github.com>
2026-05-12 08:45:45 -0400

    Update setting.c

--- source/setting.c
+++ source/setting.c
@@ -6,57 +6,42 @@
 //colors
 #define RED 0xFF0000FF
 #define BLUE 0x0000FFFF
+//font
 #include "Letter_Gothic_Std_14_Bold_png.h"
+//images
 #include "back_png.h"
 
 int e() {
-    int camZ=13.0f;
-
+    //graphics init
     GRRLIB_Init();
+    //input init
     WPAD_Init();
+    InitInput();
 
 
+    //loading font
     GRRLIB_texImg *tex_font = GRRLIB_LoadTexture(Letter_Gothic_Std_14_Bold_png);
     GRRLIB_texImg *tex_back = GRRLIB_LoadTexture(back_png);
 
     GRRLIB_InitTileSet(tex_font, 11, 24, 32);
-
-
     GRRLIB_Settings.antialias = true;
-
     GRRLIB_SetBackgroundColour(0x00, 0x00, 0x00, 0xFF);
+    GRRLIB_SetLightAmbient(0x333333FF);
 
     while(1) {
         GRRLIB_2dMode();
         WPAD_ScanPads();
         if(WPAD_ButtonsDown(0) & WPAD_BUTTON_HOME) break; //goes back to home i hope
-
-        GRRLIB_Camera3dSettings(0.0f,0.0f,camZ, 0,1,0, 0,0,0);
-        GRRLIB_SetLightAmbient(0x333333FF);
+       GRRLIB_SetLightAmbient(0x333333FF);
 
         GRRLIB_2dMode();
         //title text 
         GRRLIB_Printf(215, 20, tex_font, RED, 5, "settings");
-        GRRLIB_Printf(100, 400, tex_font, RED, 2, "return to home");
-        //need to button stuff eventuly.
-        /*
-        exit(0)
-        */
-        GRRLIB_Printf((640-(16*14))/2, 70, tex_font, RED, 3, "volume,");
-        GRRLIB_Printf((640-(16*14))/2, 150, tex_font, RED, 3, "settings");
-        GRRLIB_Printf((640-(16*14))/2, 200, tex_font, RED, 3, "rumble");
-        GRRLIB_Printf((640-(16*14))/2, 200, tex_font, RED, 3, "rumble"); //will make text etherir say on  or off
-
-        GRRLIB_Printf((640-(16*14))/2, 250, tex_font, RED, 3, "thanks to the wii hombrewing community");
-        GRRLIB_Printf((640-(16*14))/2, 300, tex_font, RED, 3, "check out GRRLIB at https://github.com/GRRLIB/GRRLIB");
-        GRRLIB_Printf((640-(16*14))/2, 350, tex_font, RED, 3, "check out source code at https://github.com/Zpeedbrew/Saiiber/");
-        //debug text neeed to make togglable
-        /*
-        GRRLIB_Printf((640-(16*14))/2, 350, tex_font, 0xFF0000FF, 2, "current version:1.6");
-        GRRLIB_Printf((640-(16*14))/2, 350, tex_font, 0xFF0000FF, 2, "rendering engine:GRRLIB");
-        */
-
-
+        GRRLIB_Printf(10, 400, tex_font, RED, 2, "return to home");
+        GRRLIB_Printf(10, 70, tex_font, RED, 3, "volume,");
+        GRRLIB_Printf(10, 150, tex_font, RED, 3, "settings");
+        GRRLIB_Printf(10, 200, tex_font, RED, 3, "rumble");
+        GRRLIB_Printf(10, 200, tex_font, RED, 3, "rumble"); //will make text etherir say on  or off
 
         GRRLIB_Render();
     }
