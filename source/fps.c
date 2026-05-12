1a7e00498b004aaf4628ebfc9ca190b4d652d996
guinea7pig <145406121+guinea7pig@users.noreply.github.com>
2026-05-12 11:03:43 -0400

    Create fps.c

---
+++ source/fps.c
@@ -0,0 +1,16 @@
+include "fps.h"
+#include <ogc/lwp_watchdog.h> 
+
+
+static u8 CalculateFrameRate(void) {
+    static u8 frameCount = 0;
+    static u32 lastTime;
+    static u8 FPS = 0;
+    const u32 currentTime = ticks_to_millisecs(gettime());
+
+    frameCount++;
+    if(currentTime - lastTime > 1000) {
+        lastTime = currentTime;
+        FPS = frameCount;
+        frameCount = 0;
+    }
