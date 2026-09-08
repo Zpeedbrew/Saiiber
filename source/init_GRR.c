d846ad6e86b628b9bca86c4bf1558bc30c1cbd04
guinea7pig <145406121+guinea7pig@users.noreply.github.com>
2026-09-08 11:31:56 -0400

    Update init_GRR.c

--- source/init_GRR.c
+++ source/init_GRR.c
@@ -1,10 +1,9 @@
 #include "input.h"
 
-void InitInput() {
+void graphics_init() {
 GRRLIB_Init();
-    GRRLIB_Settings.antialias = true;
-    GRRLIB_SetBackgroundColour(0x00, 0x00, 0x00, 0xFF); 
+GRRLIB_Settings.antialias = true;
+GRRLIB_SetBackgroundColour(0x00, 0x00, 0x00, 0xFF); 
 GRRLIB_SetLightAmbient(0x333333FF);
-}
 
-v
+}
