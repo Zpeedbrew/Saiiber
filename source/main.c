0f591e9aa884a594f48bcd6ea31b1ba0dacd454a
guinea7pig <145406121+guinea7pig@users.noreply.github.com>
2026-09-08 11:34:17 -0400

    Update main.c

--- source/main.c
+++ source/main.c
@@ -51,10 +51,13 @@ void
 int main() {
     //Scene currentScene = SCENE_MENU;
     //graphics library init
-    GRRLIB_Init();
+	graphics_init();
+    /*
+	GRRLIB_Init();
     GRRLIB_Settings.antialias = true;
     GRRLIB_SetBackgroundColour(0x00, 0x00, 0x00, 0xFF); 
     GRRLIB_SetLightAmbient(0x333333FF);
+	*/
     //audio init
     //ASND_Init();
 	//MP3Player_Init();
