32765535ec06882d2d5825f139c85f0958ada50e
guinea7pig <145406121+guinea7pig@users.noreply.github.com>
2026-09-08 11:07:49 -0400

    Update init_GRR.c

--- source/init_GRR.c
+++ source/init_GRR.c
@@ -1,1 +1,18 @@
+#include "input.h"
 
+void InitInput() {
+    WPAD_Init();
+    WPAD_SetDataFormat(WPAD_CHAN_0, WPAD_FMT_BTNS_ACC_IR);
+    WPAD_SetVRes(WPAD_CHAN_0, 640, 480);
+}
+
+void GetIRPointer(int chan, float *x, float *y) {
+    WPADData *data = WPAD_Data(chan);
+    if (data != NULL && data->ir.valid) {
+        *x = data->ir.x;
+        *y = data->ir.y;
+    } else {
+        *x = -1.0f; 
+        *y = -1.0f;
+    }
+}
