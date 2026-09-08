088d3c0242284e9f132912d2c23e7c017b1aac37
guinea7pig <145406121+guinea7pig@users.noreply.github.com>
2026-09-08 11:07:07 -0400

    Create init_GRR.h

---
+++ source/init_GRR.h
@@ -0,0 +1,17 @@
+#ifndef INIT_H
+#define INPUT_H
+
+#include <wiiuse/wpad.h>
+
+// Initialize and Update
+void InitInput();
+void UpdateInput();
+
+// Button Checks
+bool ButtonPressed(int chan, int button);
+bool ButtonHeld(int chan, int button);
+
+// IR Tracking (New)
+void GetIRPointer(int chan, float *x, float *y);
+
+#endif
