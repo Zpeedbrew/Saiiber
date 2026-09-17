fa0980ba1b7313a688686048fa0baa274741f336
guinea7pig <145406121+guinea7pig@users.noreply.github.com>
2026-09-17 14:48:43 -0400

    Update main.c

--- source/main.c
+++ source/main.c
@@ -32,13 +32,13 @@ float cursorX, cursorY;
 int main() {
     //Scene currentScene = SCENE_MENU;
     //graphics library init
-	graphics_init();
-    /*
+	//graphics_init();
+    
 	GRRLIB_Init();
     GRRLIB_Settings.antialias = true;
     GRRLIB_SetBackgroundColour(0x00, 0x00, 0x00, 0xFF); 
     GRRLIB_SetLightAmbient(0x333333FF);
-	*/
+	GRRLIB_Camera3dSettings(0,0,0,0,0,0,0,0,0,0);
     //audio init
     //ASND_Init();
 	//MP3Player_Init();
